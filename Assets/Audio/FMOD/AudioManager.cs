306afcc29ee31db2f5fa9d1b64e9f3e163ae2ca4
ShibaInuEnjoyer <asiergaraupv@gmail.com>
2025-11-29 10:37:48 +0100

    FMOD_Events + NullBanks

--- Assets/Audio/FMOD/AudioManager.cs
+++ Assets/Audio/FMOD/AudioManager.cs
@@ -1,6 +1,49 @@
 using UnityEngine;
+using System;
+using FMODUnity;
+using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
-    
+
+    public static AudioManager instance;
+
+    private EventInstance musicEventInstance;
+
+    private EventInstance timeTrialInstance;
+    private bool isTimeTrialPaused = false;
+    private int pausedTimelinePosition = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void PlayOneShot(EventReference sound, Vector3 worldpos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldpos);
+    }
+
+    private void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateInstance(musicEventReference);
+
+    }
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        return eventInstance;
+    }
+
+    public void StopMusic()
+    {
+        musicEventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
 }
--- Assets/Audio/FMOD/FMOD_Events.cs
+++ Assets/Audio/FMOD/FMOD_Events.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
-
+using UnityEngine;
+using FMODUnity;
 public class FMOD_Events : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    ///////////      Menu      ///////////
+    [field: Header("Menu")]
+    [field: SerializeField] public EventReference SelectButton { get; private set; }
+    [field: SerializeField] public EventReference ConfirmButton { get; private set; }
+    [field: SerializeField] public EventReference BackInMenu { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+
+    ///////////      Scoller      ///////////
+    [field: Header("Scroller")]
+    [field: SerializeField] public EventReference Scroll { get; private set; }
+    [field: SerializeField] public EventReference LikeAPost { get; private set; }
+    [field: SerializeField] public EventReference PostEndEffect { get; private set; }
+
+
+
+    ///////////      Minigame 1      ///////////
+    [field: Header("Minigame 1")]
+    [field: SerializeField] public EventReference LadridoPerro { get; private set; }
+    [field: SerializeField] public EventReference LanzarPelota { get; private set; }
+    [field: SerializeField] public EventReference PerroRecogePelota { get; private set; }
+    [field: SerializeField] public EventReference CogerPelotaDeVuelta { get; private set; }
+
+
+    ///////////      MiniGame 2      ///////////
+    [field: Header("Minigame 2")]
+    [field: SerializeField] public EventReference PasarToalla { get; private set; }
+
+
+
+    ///////////      Miscelanea      ///////////
+    [field: Header("Miscelanea")]
+    [field: SerializeField] public EventReference CompletarMinijuego { get; private set; }
+    [field: SerializeField] public EventReference FallarMinijuego { get; private set; }
+    [field: SerializeField] public EventReference MusicWhooshChange { get; private set; }
+
+    ///////////      GameEnd      ///////////
+    [field: Header("Game Ending")]
+    [field: SerializeField] public EventReference OutOfBattery { get; private set; }
+    [field: SerializeField] public EventReference FullOfAnxiety { get; private set; }
+
+    ///////////      OST      ///////////
+    [field: Header("OST")]
+    [field: SerializeField] public EventReference MainMenuMusic { get; private set; }
+    [field: SerializeField] public EventReference GameplayMusic { get; private set; }
+
+
+
+    ///////////      CODE      ///////////
+    public static FMOD_Events instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one FMOD_Event instance in the scene");
+        }
+        instance = this;
     }
 }
