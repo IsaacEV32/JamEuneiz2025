a6390ff5d7f9547e4cf29f2f91c52c5035b48246
IsaacEV <isaac_escudero@euneiz.com>
2025-11-29 23:09:04 +0100

    Las imagenes de los posts cambian dependiendo de si son de divertidos o depresivos.

--- Assets/Scripts/Posts/Posts.cs
+++ Assets/Scripts/Posts/Posts.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum TipoPosts
 {
@@ -6,9 +7,13 @@ public enum TipoPosts
 };
 public class Posts : MonoBehaviour
 {
-    
+    public Image imagenPrincipal;
+    public Sprite[] imagenesDisponiblesFelices;
+    public Sprite[] imagenesDisponiblesDepresivas;
+
     [SerializeField] internal TipoPosts posts;
     float numRandom = 0;
+    float numRandomPost = 0;
     [SerializeField] Scroll_Control scrollControl;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,25 +21,46 @@ public class Posts : MonoBehaviour
         scrollControl.GetThisPost(this);
         numRandom = Random.value;
         float roundedValue = Mathf.Round(numRandom * 10f) / 10f;
+
         Debug.Log(roundedValue);
         if (numRandom <= 0.5f)
         {
             this.posts = TipoPosts.Divertido;
+            numRandomPost = Random.value;
+            float roundedValueFunny = Mathf.Round(numRandomPost * 10f) / 10f;
+
+            if (numRandomPost <= 0.3)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesFelices[0];
+            }
+            else if(numRandomPost <= 0.7)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesFelices[1];
+            }
+            else if(numRandomPost <= 1.0f)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesFelices[2];
+            }
         }
         else if (numRandom > 0.5f)
         {
 
             this.posts = TipoPosts.Depresivo;
+            if (numRandomPost <= 0.3)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesDepresivas[0];
+            }
+            else if (numRandomPost <= 0.7)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesDepresivas[1];
+            }
+            else if (numRandomPost <= 1.0f)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesDepresivas[2];
+            }
         }
     }
     
-    // Update is called once per frame
-    void Update()
-    {
-
-        
-
-    }
     internal void ChangeTipe()
     {
         numRandom = Random.value;
@@ -43,11 +69,37 @@ public class Posts : MonoBehaviour
         if (roundedValue <= 0.5f)
         {
             this.posts = TipoPosts.Divertido;
+            numRandomPost = Random.value;
+            float roundedValueFunny = Mathf.Round(numRandomPost * 10f) / 10f;
+
+            if (numRandomPost <= 0.3)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesFelices[0];
+            }
+            else if (numRandomPost <= 0.7)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesFelices[1];
+            }
+            else if (numRandomPost <= 1.0f)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesFelices[2];
+            }
         }
         else if (roundedValue > 0.5f)
         {
-
             this.posts = TipoPosts.Depresivo;
+            if (numRandomPost <= 0.3)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesDepresivas[0];
+            }
+            else if (numRandomPost <= 0.7)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesDepresivas[1];
+            }
+            else if (numRandomPost <= 1.0f)
+            {
+                imagenPrincipal.sprite = imagenesDisponiblesDepresivas[2];
+            }
         }
     }
 }
