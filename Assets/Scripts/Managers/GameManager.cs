3e0d35ca58666810a50056712ea48798fb9daf5f
IsaacEV <isaac_escudero@euneiz.com>
2025-11-29 16:21:49 +0100

    Se ha añadido que se restringa el crecimiento y decrecimiento de 0 a 100 de la barra de ansiedad.
    El valor por defecto de la barra de ansiedad será de 40

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -45,6 +45,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ansiedad.value = Mathf.Clamp(ansiedad.value, 0, 99);
         ////if(juegoTerminado) return;
         //tiempoRestante -= Time.deltaTime;
 
