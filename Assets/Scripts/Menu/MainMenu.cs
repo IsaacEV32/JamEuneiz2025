603ff2767c2705ddb36cb76087b81337371f7a00
Markel16 <markel_nunez@euneiz.com>
2025-11-29 09:36:20 +0100

    menu arreglado

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
     public void Jugar()
     {
         
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("DoomScroller");
     }
 
     public void Salir()
