d4a52dbb91ab358847b7ded3457bf9e1cbe28899
Markel16 <markel_nunez@euneiz.com>
2025-11-29 09:33:15 +0100

    Menu inicial creado

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Jugar()
+    {
+        
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+
+        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
